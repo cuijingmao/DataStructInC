bdd1f27ab6ae2447bf7be72143d361d80e9f0fcf
cuijingmao <cuijingmao12@163.com>
2020-05-17 17:26:01 +0800

    利用栈实现 十进制转八进制

--- Applications/NumeratiionConvert/conversion.cpp
+++ Applications/NumeratiionConvert/conversion.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "conversion.h"
+void conversion(){
+    int N;
+    SqStack S;
+    InitStack(S);
+    scanf("%d", &N);
+    while(N){
+        Push(S,N%8);
+        N = N/8;
+    }
+    while( !StackEmpty(S)){
+        ElemType e;
+        Pop(S,e);
+        printf("%d",e);
+    }
+
+
+}
--- Applications/NumeratiionConvert/conversion.h
+++ Applications/NumeratiionConvert/conversion.h
@@ -1,16 +1,12 @@
 //
 // Created by Carey on 2020/5/17.
-//
+//  用于将十进制转化为八进制
 
 #ifndef C_CONVERSION_H
 #define C_CONVERSION_H
 
+#include "../../Stack/SqStack.h"
 
-
-class conversion {
-
-};
-
-
+void conversion();
 
 #endif //C_CONVERSION_H
