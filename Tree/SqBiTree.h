3790cd5079700e14a932179da6993f529f1e3682
cuijingmao <cuijingmao12@163.com>
2020-05-21 22:49:35 +0800

    顺序存储结构二叉树

---
+++ Tree/SqBiTree.h
@@ -0,0 +1,14 @@
+//
+// Created by Carey on 2020/5/21.
+//  二叉树的顺序存储结构
+
+#ifndef C_SQBITREE_H
+#define C_SQBITREE_H
+
+typedef int TElemType;
+
+#define MAX_TREE_SIZE 100    //二叉树的最大节点数
+typedef TElemType SqBiTree[MAX_TREE_SIZE+1];  // 0号单元存储根结点
+SqBiTree bt;
+
+#endif //C_SQBITREE_H
